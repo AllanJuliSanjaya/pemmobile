1e60832a6bbf700877ba06cbc58932956afb6c21
22091397072_Rachmadan Nauval <144086273+Rachmadan210@users.noreply.github.com>
2024-02-25 23:23:01 +0700

    Update PemMobile.dart

--- PemMobile.dart
+++ PemMobile.dart
@@ -1,38 +1,20 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(
-            'I Am Adventurer',
+          title: Text(
+            'I Am Advanturer',
             style: TextStyle(color: Colors.white),
           ),
           backgroundColor: Colors.orange[400],
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Center vertically
-          children: [
-            Image(
-              image: AssetImage('assets/pemandanganairterjun.jpg'),
-            ),
-            Image(
-              image: AssetImage('assets/pemandangankali.jpg'),
-            ),
-            Image(
-              image: AssetImage('assets/pemandangankelairpantai.jpg'),
-            ),
-          ],
+        body: Center(
+          child: Image(
+            image: AssetImage('images/logoadvanturer.png')
+          ),
         ),
         backgroundColor: Colors.white,
       ),
