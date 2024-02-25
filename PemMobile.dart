1e3234d6abdbc9cfc71ead91c83ec56d89ee7c99
Allan Juli Sanjaya <124427460+AllanJuliSanjaya@users.noreply.github.com>
2024-02-25 11:22:37 +0700

    Update PemMobile.dart

--- PemMobile.dart
+++ PemMobile.dart
@@ -5,18 +5,25 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: 
-          const Text(
+          title: const Text(
             'I Am Adventurer',
             style: TextStyle(color: Colors.white),
           ),
           backgroundColor: Colors.orange[400],
         ),
-        body:
-        const Center(
-          child: Image(
-              image: AssetImage('images/logoadventurer.png')
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Center vertically
+          children: [
+            Image(
+              image: AssetImage('assets/images/pemandanganairterjun.png'),
+            ),
+            Image(
+              image: AssetImage('assets/images/pemandangankali.png'),
+            ),
+            Image(
+              image: AssetImage('assets/images/pemandangankelairpantai.png'),
+            ),
+          ],
         ),
         backgroundColor: Colors.white,
       ),
